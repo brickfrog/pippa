36e5ff9dd45f63ba3e56357d3ede0eda25de4894
Justin <9146678+brickfrog@users.noreply.github.com>
2026-05-31 12:04:22 -0400

    perf(styling): speed up Style render

---
+++ src/styling/render_perf_native.c
@@ -0,0 +1,7 @@
+#include <time.h>
+
+long long pippa_styling_perf_now_us(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (long long)ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
+}
